4e426e14f8eacf808feab15562274a8e24d7d94f
Amrita Reji <amritareji@Amritas-MacBook-Pro.local>
2026-01-28 11:41:33 +0530

    hi3333

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Cupcake {
 
@@ -7,25 +6,11 @@ public class Cupcake {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] calories = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            calories[i] = sc.nextInt();
-        }
-
-        // Sort in ascending order
-        Arrays.sort(calories);
-
-        // Reverse to get descending order
-        for (int i = 0; i < n / 2; i++) {
-            int temp = calories[i];
-            calories[i] = calories[n - 1 - i];
-            calories[n - 1 - i] = temp;
-        }
-
         long miles = 0;
+
         for (int i = 0; i < n; i++) {
-            miles += calories[i] * (1L << i);
+            long c = sc.nextLong();
+            miles += c * (1L << i);
         }
 
         System.out.print(miles);
